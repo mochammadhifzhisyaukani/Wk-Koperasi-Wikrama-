609232a0c137f15a8bd7cac0b62ca97254a8658b
Mochammad Hifzhisyaukani <mochammadhifzhisyaukani@smkwikrama.sch.id>
2026-05-18 13:07:35 +0700

    hifzhi: navbar, footer, page welcome

---
+++ src/js/component/footer.js
@@ -0,0 +1,28 @@
+let footer = document.getElementById('footer');
+
+footer.innerHTML = `
+  <footer class="footer-section py-4">
+    <div class="container text-center">
+      <!-- Nama koperasi -->
+      <h3>Koperasi Wikrama</h3>
+
+      <!-- Alamat -->
+      <p class="mt-2">
+        SMK Wikrama Bogor<br>
+        Jl. Raya Wangun, Kelurahan Sindangsari,<br>
+        Kecamatan Bogor Timur, Kota Bogor, Jawa Barat 16146
+      </p>
+
+      <!-- Link sederhana -->
+      <div class="mt-3">
+        <a href="#" class="text-decoration-none mx-2">Produk</a>
+        <a href="#" class="text-decoration-none mx-2">Laporan</a>
+        <a href="#" class="text-decoration-none mx-2">Kontak</a>
+      </div>
+
+      <!-- Hak cipta -->
+      <hr class="mt-4 mb-2">
+      <small>© 2026 Koperasi Wikrama. All rights reserved</small>
+    </div>
+  </footer>
+`;
---
+++ src/js/component/navbar.js
@@ -0,0 +1,21 @@
+let navbar = document.getElementById("navbar");
+
+navbar.innerHTML = `           <nav class="navbar navbar-expand-lg">
+                <div class="container-sm mt-3">
+                    <a class="navbar-brand" href="#"><img src="/src/assets/images/Logo Kw hifzhi.png" alt="" height="100px" style="margin-top: 13px;" ></a>
+                    <!-- <span>Koperasi SMK Wikrama Bogor</span> -->
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav ms-auto gap-3">
+                            <a class="nav-link" href="#">Produk</a>
+                            <a class="nav-link" href="#">Layanan</a>
+                            <a class="nav-link" href="#">Pengumuman</a>
+                            <a class="nav-link" href="#">Kontak</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>`;
